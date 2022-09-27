4a4e8f7b05f85876df049a5b2459b7b7ca0e7a30
nceccanti <nicceccanti@gmail.com>
2022-09-27 10:49:56 -0600

    started editted knn

--- KNN.py
+++ KNN.py
@@ -1,4 +1,12 @@
 class KNN:
+    def __init__(self, df, test, test_y, train, train_y, k):
+        self.df = df
+        self.test = test
+        self.test_y = test_y
+        self.train = train
+        self.train_y = train_y
+        self.k = k
+
     def lpNorm(self, x, y, p):
         """
 
@@ -23,7 +31,48 @@ class KNN:
         r = 1 / (2 * sigma)
         return math.exp(-r * lpNorm(x, y, 2))
 
-    def predict(self, test, train, train_y, k, isClassification, bandwidth):
+    def predictInstance(self, testInstance):
+        neighbors = []
+        classes = []
+        indices = []
+        for i, row in train.iterrows():
+            dist = lpNorm(np.array(row), np.array(testInstance), 2)
+            neighbors.append(dist)
+            classes.append(train_y[i])
+            indices.append(i)
+
+        for i in range(len(neighbors)):
+            for j in range(0, len(neighbors) - i - 1):
+                if neighbors[j] > neighbors[j + 1]:
+                    neighbors[j], neighbors[j + 1] = neighbors[j + 1], neighbors[j]
+                    classes[j], classes[j + 1] = classes[j + 1], classes[j]
+                    indices[j], indices[j + 1] = indices[j + 1], indices[j]
+
+        nearestNeighbors = classes[:k]
+        nearestIndices = indices[:k]
+
+        if isClassification:
+            vote = np.unique(nearestNeighbors)
+            count = []
+            for i in range(len(vote)):
+                count.append(0)
+
+            for i in nearestNeighbors:
+                for j, cl in enumerate(vote):
+                    if i == cl:
+                        count[j] += 1
+            return vote[np.argmax(count)]
+        else:
+            nom = 0
+            dom = 0
+            for i in nearestIndices:
+                kern = kernel(train.loc[i], testInstance, bandwidth)
+                nom += kern * train_y[i]
+                dom += kern
+
+            return (nom / dom)
+
+    def kknRegular(self, test, test_y, train, train_y, k, isClassification, bandwidth, error):
         """
 
         :param test:
@@ -72,12 +121,9 @@ class KNN:
                     kern = kernel(train.loc[i], testInstance, bandwidth)
                     nom += kern * train_y[i]
                     dom += kern
-                avg = 0
-                for i in nearestIndices:
-                    avg += train_y[i]
-                avg /= k
-                print(nom / dom)
-                print(avg)
-                print()
+
                 predictions.append(nom / dom)
         return predictions
+
+    def kknEdited(self, df):
+        for i, row in df.iterrows:
